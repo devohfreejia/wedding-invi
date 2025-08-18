9153a490c9450b2a1b39fc895e205b48a0def185
devForJaja <ohfreejia@gmail.com>
2025-08-18 23:01:41 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 export default {
@@ -9,14 +9,6 @@ export default {
     postcss: true
   }),
   kit: {
-    adapter: adapter({
-      // 빌드 결과물이 public이 아닌 'build' 폴더에 생성되도록 설정
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    prerender: {
-      default: true  // 모든 페이지를 빌드시 미리 렌더링
-    }
+    adapter: adapter()
   }
 };
